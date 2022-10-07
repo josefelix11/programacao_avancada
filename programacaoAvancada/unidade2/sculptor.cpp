100ebd3ff723f5bd6d687dc992401688dd311460
josefelix22 <felix.anselmo.103@ufrn.edu.br>
2022-10-07 14:31:59 -0300

    Começando Projeto de Programação Avançada.

---
+++ programacaoAvancada/unidade2/sculptor.cpp
@@ -0,0 +1,11 @@
+#include "sculptor.h"
+#include "cmath"
+#include "iostream"
+
+//Nome da Classe Sculptor
+
+Sculptor::Sculptor(int _nx,int _ny, int -nz){
+    nx = _nx;
+    ny = _ny;
+    nz = _nz;
+}
