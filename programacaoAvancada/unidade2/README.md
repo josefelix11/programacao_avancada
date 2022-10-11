a368f7bd9aaa71e31428bf0542e7bb6e3ddac8a6
josefelix22 <felix.anselmo.103@ufrn.edu.br>
2022-10-10 23:34:20 -0300

    Começando a implementação do metodo que cria o Voxel:

--- programacaoAvancada/unidade2/README.md
+++ programacaoAvancada/unidade2/README.md
@@ -1,3 +1,6 @@
 # Projeto Programação Avançada
 
-'#RRGGBB`Este repositório destina-se ao armazenamento e atualização dos dados referentes ao projeto da disciplina de Programação Avançada.
+Este repositório destina-se ao armazenamento e atualização dos dados referentes ao projeto da disciplina de Programação Avançada.
+
+##Objetivo
+Criar uma ferramenta que possa desenvolver figuras ao mesmo estilo minecraft, no qual possam ser feita figuras através de um molde quadrado.
--- programacaoAvancada/unidade2/sculptor.cpp
+++ programacaoAvancada/unidade2/sculptor.cpp
@@ -1,11 +1,86 @@
+//sculptor.cpp
 #include "sculptor.h"
-#include "cmath"
-#include "iostream"
+#include <cmath>
+#include <iostream>
 
-//Nome da Classe Sculptor
+Scupltor::Sculptor(int _nx, int _ny, int _nz) {
+  // Construtor da classe
+  std::cout << "Inciando os Construtores!! " << std::endl
+  this->x = _nx;
+  this->y = _ny;
+  this->z = _nz;
+  r = g = b = a = 0.5;
+  side = 0.5;
 
-Sculptor::Sculptor(int _nx,int _ny, int -nz){
-    nx = _nx;
-    ny = _ny;
-    nz = _nz;
-}
+
+  //alocando Dinanicamente o cubo
+  v = new Voxel**[x];
+
+  for(int i = 0;i < x;i++){
+      v[i] = new Voxel*[x];
+  }
+
+  for(int i = 0;i < x;i++){
+      for(j = 0;j < y;j++){
+          V[i][j] = new Voxel*[z];
+
+      }
+  }
+};
+
+Scupltor::~Sculptor(int _nx, int _ny, int _nz) {
+  //Destrutor
+  for(int i = 0; i < x; i++){
+      for(int j = 0; j < y; j++){
+         delete v[i][j];
+      }
+  }
+  for(int i = 0;i < x;i++){
+      delete v[i];
+  }
+
+  delete v;
+  cout << "Destrutor Sculptor"
+};
+
+void setColor(float r, float g, float b, float a){
+  this->r = r; //red
+  this->g = g; //green
+  this->b = b; //blue
+  this->a = a; //Transparência
+};
+
+void putVoxel(int _nx, int _ny, int _nz){
+  if((_nx < x && _nx > 0) && (_ny < y && _ny > 0) && (_nz < z && _nz > 0)){
+      v[_nx][_ny][_nz].r = r;
+      v[_nx][_ny][_nz].g = g;
+      v[_nx][_ny][_nz].b = b;
+      v[_nx][_ny][_nz].a = a;
+      v[_nx][_ny][_nz].isOn = true;
+
+  }
+};
+void cutVoxel(int x, int y, int z){
+  
+};
+void putBox(int x0, int x1, int y0, int y1, int z0, int z1){
+  
+};
+void cutBox(int x0, int x1, int y0, int y1, int z0, int z1){
+  
+};
+void putSphere(int xcenter, int ycenter, int zcenter, int radius){
+  
+};
+void cutSphere(int xcenter, int ycenter, int zcenter, int radius){
+  
+};
+void putEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int ry, int rz){
+  
+};
+void cutEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int ry, int rz){
+  
+};
+void writeOFF(const char* filename){
+  
+};
