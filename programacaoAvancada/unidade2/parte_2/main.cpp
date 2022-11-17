da1f01e4c1752e8964253d167f0de225385a2476
jose_felix <113564408+josefelix11@users.noreply.github.com>
2022-11-17 09:58:49 -0300

    Update main.cpp

--- programacaoAvancada/unidade2/parte_2/main.cpp
+++ programacaoAvancada/unidade2/parte_2/main.cpp
@@ -10,45 +10,6 @@ using namespace std;
 
 int main()
 {
-
-    /* ---- CORTANDO BLOCOS ----
-
-    for(int i = 30; i <= 70; i++){
-        int j = i;
-        nave.cutVoxel(i,85,j);
-        nave.cutVoxel(i,15,j);
-
-        nave.setColor(0,0,0,0);
-        nave.putVoxel(i,85,j);
-        nave.setColor(0,0,0,0);
-        nave.putVoxel(i,15,j);
-    }
-
-    for(int i = 30; i <= 70; i++){
-        int j = 100-i;
-        nave.cutVoxel(i,85,j);
-        nave.cutVoxel(i,15,j);
-
-        nave.setColor(0,0,0,0);
-        nave.putVoxel(i,85,j);
-        nave.setColor(0,0,0,0);
-        nave.putVoxel(i,15,j);
-
-    }
-
-    for(int i = 30; i <= 70; i++){
-        nave.cutVoxel(i,85,50);
-        nave.cutVoxel(i,15,50);
-
-        nave.setColor(0,0,0,0);
-        nave.putVoxel(i,85,50);
-        nave.setColor(0,0,0,0);
-        nave.putVoxel(i,15,50);
-    }
-
-
-    */
-
     Sculptor *nave;
     Interpretador parser;
     vector<FiguraGeometrica*> figuras;
